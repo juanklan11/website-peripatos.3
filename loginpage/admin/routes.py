1849acdf82016c73ec9f2000669d3bed5825ab39
juanklan11 <57101853+juanklan11@users.noreply.github.com>
2020-04-27 23:09:27 +0200

    first commit

--- loginpage/admin/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, request, Blueprint, url_for, flash, redirect
-from loginpage.models import Post
-from loginpage.posts.forms import FindForm
-from flask_login import login_user, current_user
-
-administrador = Blueprint('admin', __name__)
-
-# @administrador.route("/register_roles", methods=['GET', 'POST'])
-# def register():
